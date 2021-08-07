2a965f803cdd3e54d7d0d20095eee54c1248de00
bodiddlie <bodiddlie@gmail.com>
2021-08-07 16:25:05 -0700

    add log out button to header

--- src/header.tsx
+++ src/header.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { MdMenu } from 'react-icons/md';
 
-import {MdMenu} from 'react-icons/md';
+import { useAuth } from './auth';
 
 export default function Header() {
-	return (
-		<div className="p-2 flex w-full justify-between bg-blue-600">
-			<MdMenu />
-			<Link to="/login">Login</Link>
-		</div>
-	)
+  const auth = useAuth();
+
+  return (
+    <div className="p-2 flex w-full justify-between bg-blue-600">
+      <MdMenu />
+      {auth?.user ? (
+        <button type="button" onClick={() => auth?.signout()}>
+          Log Out
+        </button>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
+    </div>
+  );
 }
