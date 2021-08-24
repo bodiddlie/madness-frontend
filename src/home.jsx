83e9a6f98d28681e5fc618103cfb635290a7ca81
bodiddlie <bodiddlie@gmail.com>
2021-08-23 18:05:24 -0700

    add querying profile and redirect if sorted

--- src/home.jsx
+++ src/home.jsx
@@ -2,9 +2,37 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 import { useAuth } from './auth';
+import { getUserProfile } from './api';
 
 export default function Home() {
+  const [profile, setProfile] = React.useState(null);
   const auth = useAuth();
 
-  return auth.user ? <Redirect to="/list" /> : <Redirect to="/login" />;
+  React.useEffect(() => {
+    if (auth.user) {
+      getUserProfile().then((profile) => {
+        setProfile(profile);
+      });
+    }
+  }, [auth.user]);
+
+  if (!auth.user) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div>
+      {!profile ? (
+        <div>Loading...</div>
+      ) : (
+        <React.Fragment>
+          {profile.isSorted ? (
+            <Redirect to="/focus" />
+          ) : (
+            <Redirect to="/list" />
+          )}
+        </React.Fragment>
+      )}
+    </div>
+  );
 }
